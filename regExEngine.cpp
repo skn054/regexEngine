ba59cd5ce38aba8cc1cbaea6d193aa23a19e81f7
recursion <skn054@gmail.com>
2025-07-11 19:22:07 +0530

    v2 infixToPostFix

--- regExEngine.cpp
+++ regExEngine.cpp
@@ -2,78 +2,136 @@
 #include <stack>
 using namespace std;
 
-struct state{
+struct state
+{
     int c;
     struct state *out;
     struct state *out1;
 };
 
-int getPrecedence(char ch){
+int getPrecedence(char ch)
+{
     switch (ch)
     {
-        case '(' || ')':
-            return 4;
-        
-        case '*':
-        case '+':
-        case '?':
-            return 3;
-        case '.':
-             return 2;
-        case '|':
-            return 1;
-        default:
-            break;
+
+    case '*':
+    case '+':
+    case '?':
+        return 3;
+    case '.':
+        return 2;
+    case '|':
+        return 1;
+    default:
+        return 0;
     }
 }
 
-bool checkPriority(char currElement,char topOfStack){
-       
-        if(getPrecedence(currElement)>getPrecedence(topOfStack)){
-            return true;
-        }else{
-            return false;
-        }
+bool checkPriority(char currElement, char topOfStack)
+{
+
+    if (getPrecedence(currElement) > getPrecedence(topOfStack))
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
 }
-void infixtoPstfix(string expr){
+
+string getModifiedInputString(string expression)
+{
+    int strLength = expression.length();
+    if (strLength <= 1)
+    {
+        return " ";
+    }
+
+    string resultExpression;
+    resultExpression.push_back(expression[0]);
+
+    for (int i = 1; i < strLength; i++)
+    {
+        char prevCh = expression[i - 1];
+        char currCh = expression[i];
+
+        if ((isalnum(prevCh) || prevCh == ')' || prevCh == '*' || prevCh == '?' || prevCh == '+') &&
+            (isalnum(currCh) || currCh == '('))
+        {
+
+            resultExpression.push_back('.');
+        }
+        resultExpression.push_back(currCh);
+    }
+    return resultExpression;
+}
+void infixtoPstfix(string expr)
+{
     stack<char> s;
-    int strLength = expr.length();
+    string modifiedExpression = getModifiedInputString(expr);
+    int strLength = modifiedExpression.length();
+    if (strLength <= 1 && isalnum(modifiedExpression[0]))
+        return;
     string result;
-    for(int i=0;i<strLength;i++){
-        // cout << i;
-        if(expr[i]>='a' && expr[i]<='z'){
-            result.push_back(expr[i]);
-            
-        }else{
-            if(s.empty()){
-                s.push(expr[i]);
-            }
-            else if(checkPriority(expr[i],s.top())){ // priority of current element greater than top of stack, then push current Element
-                s.push(expr[i]);
-            }else{
-                while(!s.empty()  && (getPrecedence(s.top()) <= getPrecedence(expr[i]))){
+    for (int i = 0; i < strLength; i++)
+    {
+
+        if (modifiedExpression[i] >= 'a' && modifiedExpression[i] <= 'z')
+        {
+            result.push_back(modifiedExpression[i]);
+        }
+        else if (modifiedExpression[i] == '(')
+        {
+            s.push(modifiedExpression[i]);
+        }
+        else if (modifiedExpression[i] == ')')
+        {
+
+            while (!s.empty() && s.top() != '(')
+            {
                 char ch = s.top();
                 result.push_back(ch);
                 s.pop();
-
+            }
+            if (!s.empty())
+            {
+                s.pop();
             }
         }
-       
+        else
+        {
+
+            while (!s.empty() && (getPrecedence(modifiedExpression[i]) <= getPrecedence(s.top())))
+            {
+                char ch = s.top();
+                result.push_back(ch);
+                s.pop();
+            }
+            s.push(modifiedExpression[i]);
+        }
     }
+
+    while (!s.empty())
+    {
+        char ch = s.top();
+        result.push_back(ch);
+        s.pop();
+    }
+    cout << result << endl;
 }
 
-while(!s.empty()){
-    char ch = s.top();
-    result.push_back(ch);
-    s.pop();
-}
-    cout << result;
-}
+int main()
+{
+    string s = "a|bc";
+    string s1 = "a|bc";     // Should be abc.|
+    string s2 = "a(b|c)*d"; // Should be abc|*.d.
+    string s3 = "ab|c";
 
-
-int main(){
-    string s = "a|b.c";
+    // cout << getModifiedInputString("a.b|c");
     infixtoPstfix(s);
+    infixtoPstfix(s1);
+    infixtoPstfix(s2);
+    infixtoPstfix(s3);
     return 0;
 }
-
